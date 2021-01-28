58ba02abb285e8e0ec65a63124801bf180d5bae0
libsgh <woiyyng@gmail.com>
2021-01-28 18:25:34 +0800

    update
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- src/main/java/com/github/libsgh/Util.java
+++ src/main/java/com/github/libsgh/Util.java
@@ -8,6 +8,9 @@ public class Util {
 	public static String getSidFromCookie(HttpServletRequest request) {
 		Cookie[] cookies = request.getCookies();
 		String sid = null;
+		if(cookies == null) {
+			return sid;
+		}
 		for (Cookie cookie : cookies) {
 			if("sid".equals(cookie.getName())) {
 				sid = cookie.getValue();
